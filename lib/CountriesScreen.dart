1c28e4005aa12f74a3c2141bacdba630c0d61de9
edwardbayron <edwardbayron@gmail.com>
2024-09-14 23:05:22 +0300

    showing the list of capital and png link

--- lib/CountriesScreen.dart
+++ lib/CountriesScreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart';
 import 'package:logger/logger.dart';
+import 'package:vool_test_project/models/CountryDataModel.dart';
 import 'package:vool_test_project/widgets/CountryItemWidget.dart';
 
 import 'helpers/Request.dart';
@@ -23,6 +24,7 @@ class CountriesScreen extends StatefulWidget {
 
 class _CountriesScreenState extends State<CountriesScreen> {
 
+  //late Future<List<CountryDataModel>> futureCountryData;
 
   // final response = await Request.Post('https://restcountries.com/v3.1/all?fields=name,flag,flags,capital,car,languages', {
   //   'parameters': {
@@ -59,6 +61,11 @@ class _CountriesScreenState extends State<CountriesScreen> {
         (i) => CountryListItem("Estonia", 1, "Tallinn", 10000, "EST", "left")
   );
 
+  @override
+  void initState()  {
+    futureCountries = fetchCountriesData();
+    //futureCountryData = fetchCountries();
+  }
 
   Future<http.Response> requestCountriesData() async {
     final response = await http.get(Uri.parse('https://restcountries.com/v3.1/all?fields=name,flag,flags,capital,car,languages'));
@@ -71,48 +78,119 @@ class _CountriesScreenState extends State<CountriesScreen> {
     return response;
   }
 
+  Future<List<CountryModel>> fetchCountriesData() async {
+    final response = await http.get(Uri.parse('https://restcountries.com/v3.1/all?fields=name,flag,flags,capital,car,languages'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data.map((json) => CountryModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load countries');
+    }
+  }
+
+  late Future<List<CountryModel>> futureCountries;
+
   @override
   Widget build(BuildContext context) {
-    Logger elogger = new Logger();
-    elogger.d(requestCountriesData());
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        // Let the ListView know how many items it needs to build.
-        itemCount: items.length,
-        // Provide a builder function. This is where the magic happens.
-        // Convert each item into a widget based on the type of item it is.
-        itemBuilder: (context, index) {
+      body: Center(
+        child: FutureBuilder<List<CountryModel>>(
+          future: futureCountries,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<CountryModel> countries = snapshot.data!;
 
+              return ListView.builder(
+                itemCount: countries.length,
+                itemBuilder: (context, index) {
+                  CountryModel country = countries[index];
+                  return ListTile(
+                    title: Text(country.capital ?? 'Unknown Capital'),
+                    subtitle: Text('Flag PNG: ${country.pngFlag ?? 'N/A'}'),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
 
-          final item = items[index];
-
-          return CountryItemWidget(
-            capitalName: item.capitalName,
-            image: item.flag,
-          );
-        },
+            return const CircularProgressIndicator();
+          },
+        ),
       ),
-
-      //body: Container(
-      //  child:
-        // child: Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //
-        //
-        //
-        //   ],
-        // ),
-      //),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: (),
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
     );
   }
 }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  //       title: Text(widget.title),
+  //     ),
+  //     // body: ListView.builder(
+  //     //   // Let the ListView know how many items it needs to build.
+  //     //   itemCount: items.length,
+  //     //   // Provide a builder function. This is where the magic happens.
+  //     //   // Convert each item into a widget based on the type of item it is.
+  //     //   itemBuilder: (context, index) {
+  //     //
+  //     //
+  //     //     final item = items[index];
+  //     //
+  //     //     return CountryItemWidget(
+  //     //       capitalName: item.capitalName,
+  //     //       image: item.flag,
+  //     //     );
+  //     //   },
+  //     // ),
+  //     body: Center(
+  //       child: FutureBuilder<List<CountryDataModel>>(
+  //         future: futureCountryData,  // Now it's a Future<List<CountryDataModel>>
+  //         builder: (context, snapshot) {
+  //           if (snapshot.hasData) {
+  //             Logger().e("Data loaded");
+  //
+  //             // Example: Displaying the first country’s capital
+  //             return ListView.builder(
+  //               itemCount: snapshot.data?.length,
+  //               itemBuilder: (context, index) {
+  //                 final country = snapshot.data?[index];
+  //                 return ListTile(
+  //                   title: Text(snapshot.data?.first.nativeCommonName ?? 'Unknown'),
+  //                   subtitle: Text('Capital: ${country?.capital?.first.toString() ?? 'N/A'}'),
+  //                 );
+  //               },
+  //             );
+  //           } else if (snapshot.hasError) {
+  //             Logger().e("Data errror");
+  //             return Text('${snapshot.error}');
+  //           }
+  //           return const CircularProgressIndicator();
+  //         },
+  //       ),
+  //     ),
+  //
+  //     //body: Container(
+  //     //  child:
+  //       // child: Column(
+  //       //   mainAxisAlignment: MainAxisAlignment.center,
+  //       //   children: <Widget>[
+  //       //
+  //       //
+  //       //
+  //       //   ],
+  //       // ),
+  //     //),
+  //     // floatingActionButton: FloatingActionButton(
+  //     //   onPressed: (),
+  //     //   tooltip: 'Increment',
+  //     //   child: const Icon(Icons.add),
+  //     // ),
+  //   );
+  // }
---
+++ lib/models/CountryDataModel.dart
@@ -0,0 +1,60 @@
+// class CountryDataModel {
+//   final String pngFlag;
+//   final String svgFlag;
+//   final String altFlagText;
+//   final String common;
+//   final String officialName;
+//   final String? nativeCommonName;
+//   final String? nativeOfficialName;
+//   final List<dynamic> capital;
+//   final String language;
+//   final String flagEmoji;
+//   final String carSign;
+//   final String driveSide;
+//
+//   const CountryDataModel({
+//     required this.pngFlag,
+//     required this.svgFlag,
+//     required this.altFlagText,
+//     required this.common,
+//     required this.officialName,
+//     required this.nativeCommonName,
+//     required this.nativeOfficialName,
+//     required this.capital,
+//     required this.language,
+//     required this.flagEmoji,
+//     required this.carSign,
+//     required this.driveSide,
+//   });
+//
+//   factory CountryDataModel.fromJson(dynamic json) {
+//     return CountryDataModel(
+//       pngFlag: json['flags']['png'] as String,
+//       svgFlag: json['flags']['svg'] as String,
+//       altFlagText: json['flags']['alt'] as String? ?? '',
+//       common: json['name']?['common'] as String ?? '',
+//       officialName: json['name']['official'] as String,
+//       nativeCommonName: json['name']['nativeName']['eng']?['common'],
+//       nativeOfficialName: json['name']?['nativeName']?['eng']?['official'] ?? 'test',
+//       capital: (json['capital']),
+//       language: json['languages']['eng'] ?? 'testLanguage',
+//       flagEmoji: json['flag'] ?? 'testFlagEmoji',
+//       carSign: json['car']['signs'][0] ?? 'testCarSign',
+//       driveSide: json['car']['side'] ?? 'testDriveSide',
+//     );
+//   }
+// }
+
+class CountryModel {
+  final String? capital;
+  final String? pngFlag;
+
+  CountryModel({required this.capital, required this.pngFlag});
+
+  factory CountryModel.fromJson(Map<String, dynamic> json) {
+    return CountryModel(
+      capital: json['capital'] != null && (json['capital'] as List).isNotEmpty ? json['capital'][0] : 'N/A',
+      pngFlag: json['flags']?['png'] as String?,
+    );
+  }
+}
